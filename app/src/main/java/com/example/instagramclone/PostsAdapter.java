c4fa4d8f80b6998b4f280e5b5588d5ad600ee5a3
sivakundana <sivakundana@gmail.com>
2021-03-05 22:14:18 -0600

    message

---
+++ app/src/main/java/com/example/instagramclone/PostsAdapter.java
@@ -0,0 +1,85 @@
+package com.example.instagramclone;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.parse.ParseFile;
+
+import java.util.List;
+
+public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> {
+    private Context context;
+    private List<Post> posts;
+
+    public PostsAdapter(Context context, List<Post> posts) {
+        this.context = context;
+        this.posts = posts;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Post post = posts.get(position);
+        holder.bind(post);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return posts.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView tvUsername;
+        private ImageView ivimage;
+        private TextView tvDescription;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvUsername = itemView.findViewById(R.id.tvUsername);
+            ivimage = itemView.findViewById(R.id.ivimage);
+            tvDescription = itemView.findViewById(R.id.tvDescription);
+
+        }
+
+        public void bind(Post post) {
+            //bind the post data to the view elements
+            tvDescription.setText(post.getDescription() );
+            tvUsername.setText(post.getUser().getUsername());
+            //first check if post has a valid image
+            ParseFile image = post.getImage();
+            if (image != null) {
+                Glide.with(context).load(post.getImage().getUrl()).into(ivimage);
+            }
+        }
+    }
+
+
+    // Clearing the recycler
+    public void clear() {
+        //items.clear();
+        posts.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+
+    public void addAll(List<Post> list) {
+
+        posts.addAll(list);
+        notifyDataSetChanged();
+    }
+}
